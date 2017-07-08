41ed9102815ed87813a56514a1341ed9978fdfba
Mahendran Narayanan <mahendranNNM@gmail.com>
2017-07-08 10:05:30 +0530

    Update image_scraper.py

--- image_scraper.py
+++ image_scraper.py
@@ -7,7 +7,7 @@ import requests
 """
    The url here is the google image search url which can be copied after searching for a particular image
 """
-url='https://www.google.co.in/search?q=cars&rlz=1C1CHZL_enIN742IN742&source=lnms&tbm=isch&sa=X&ved=0ahUKEwikn5e06vjUAhWKto8KHcOTBnsQ_AUICigB&biw=1366&bih=638'
+url='' # Enter your url here
 response=requests.get(url)
 page=response.content
 soup=BeautifulSoup(page)
