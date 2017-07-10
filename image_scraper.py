c6a3b424ea178126516e446c6a68b4a69d09d7e3
Mahendran Narayanan <mahendranNNM@gmail.com>
2017-07-10 19:27:15 +0530

    Update image_scraper.py

--- image_scraper.py
+++ image_scraper.py
@@ -5,17 +5,17 @@ import os
 import re
 import requests
 """
-   The url here is the google image search url which can be copied after searching for a particular image
+    The url here is the google image search url which can be copied after searching for a particular image
 """
-url='' # Enter your url here
+url=''  # Enter your url here
 response=requests.get(url)
 page=response.content
 soup=BeautifulSoup(page)
-"""
-This program can scrap only first 12 - 13 images beacuse of the string limit in naming it
-"""
-co='a';
+count=1
+n=int(raw_input("Enter the number of images to download (limit<20) :"))
 for i in soup.findAll("img",src=True):
-    urllib.urlretrieve(i['src'],"a"+co+".png")
-    co=co+'1'
-    
+    if(count<=n):
+        urllib.urlretrieve(i['src'],"image_"+str(count)+".png")
+        count+=1
+    else:
+        break
